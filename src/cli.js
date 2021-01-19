df4b6422be62eca7cf27a4650f9683dded8099ce
LenaRib <poddubnayaeo@gmail.com>
2021-01-19 14:15:09 +0100

    resolve evenGame

--- src/cli.js
+++ src/cli.js
@@ -11,4 +11,37 @@ export function hello() {
 
 export function evenGame() {
   hello();
+  let randomNum;
+  let notEven;
+  let correct = 1;
+
+  console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
+  for (correct = 0; correct < 3;) {
+    randomNum = Math.floor(Math.random() * 100 + 1);
+    notEven = randomNum % 2;
+    console.log('Question: %d', randomNum);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === 'yes') {
+      if (!notEven) {
+        console.log('Correct!');
+        correct += 1;
+      } else {
+        console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, %s!', name);
+        correct = 0;
+      }
+    } else if (userAnswer === 'no') {
+      if (notEven) {
+        console.log('Correct!');
+        correct += 1;
+      } else {
+        console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.\nLet\'s try again, %s!', name);
+        correct = 0;
+      }
+    } else {
+      console.log('Not correct input, please, try again');
+      correct = 0;
+    }
+  }
+  if (correct === 3) console.log('Congratulations, %s!', name);
 }
