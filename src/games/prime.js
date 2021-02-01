e6b380da3e897b0fea7dae0c56bd4e4df7224a1e
LenaRib <poddubnayaeo@gmail.com>
2021-02-02 00:28:03 +0100

    refactoring prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,21 +1,19 @@
 import { cons } from '@hexlet/pairs';
-import playGame from '..';
+import { playGame } from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const min = 1;
 const max = 100;
-
-const isPrime = (num) => {
-  if (num < 2) return false;
-  for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0) return false;
-  }
-  return true;
-};
+let correctAnswer = '';
 
 const getAnswer = () => {
   const question = Math.floor(Math.random() * (max - min + 1)) + min;
-  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  if (question < 2) correctAnswer = 'no';
+
+  for (let i = 2; i <= question / 2; i += 1) {
+    if (question % i === 0) correctAnswer = 'no';
+  }
+  correctAnswer = 'yes';
   return cons(question, correctAnswer);
 };
 
