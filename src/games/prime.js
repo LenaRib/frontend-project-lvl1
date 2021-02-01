6ecfe636bb0fe1b25f747eca95b7542398ec4a43
LenaRib <poddubnayaeo@gmail.com>
2021-02-02 00:20:16 +0100

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,22 @@
+import { cons } from '@hexlet/pairs';
+import playGame from '..';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const min = 1;
+const max = 100;
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const getAnswer = () => {
+  const question = Math.floor(Math.random() * (max - min + 1)) + min;
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return cons(question, correctAnswer);
+};
+
+export default () => playGame(description, getAnswer);
